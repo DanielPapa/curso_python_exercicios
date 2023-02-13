e4c5981f47dd3dbcc1ae13fdb7864172db2e2079
Daniel Papa <almeidapapa@gmail.com>
2023-02-13 18:21:28 -0500

    exercicio 2 - aula 4

---
+++ aula 04.1 - pratica de variaveis.py
@@ -0,0 +1,26 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> var_inteira = 49
+>>> var_float = 5.5
+>>> var_bool = False
+>>> var_dicionario = {'colunaA' : 10, 'colunaB1' : "teste"}
+>>> var_inteira
+49
+>>> var_string = "Regis"
+>>> var_string1 =
+SyntaxError: incomplete input
+>>> var_string1 = "Marcus"
+>>> var_string2 = "Daniel"
+>>> var_dicionario
+{'colunaA': 10, 'colunaB1': 'teste'}
+>>> var_dicionario['colunaB']
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    var_dicionario['colunaB']
+KeyError: 'colunaB'
+>>> var_dicionario['colunaB1']
+'teste'
+>>> type(var_string1)
+<class 'str'>
+>>> var_string2
+'Daniel'
