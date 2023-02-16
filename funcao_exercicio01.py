fdb213c1b1e9dbe3adcd69b39170a435459323d6
Daniel Papa <almeidapapa@gmail.com>
2023-02-16 18:18:09 -0500

    funcao
    teste

---
+++ funcao_exercicio01.py
@@ -0,0 +1,9 @@
+def funcao_media(num01, num02, num03, num04, num05):
+    media = (num01 + num02 + num03 + num04 + num05)/5
+    return media
+
+def funcao_mediana(num01, num02, num03, num04, num05):
+    mediana = num03
+    return mediana
+
+
