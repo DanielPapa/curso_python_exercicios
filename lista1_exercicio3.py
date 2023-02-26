99fdf831c5a8d79549bffeb0f353ef43ef53fd0b
Daniel Papa <almeidapapa@gmail.com>
2023-02-25 22:01:55 -0500

    Exercício 03 - Lista 01

---
+++ lista1_exercicio3.py
@@ -0,0 +1,22 @@
+## WS PRIME - LISTA DE EXERCÍCIOS Nº1 ##
+
+# EXERCÍCIO 03 #
+
+def soma(n1,n2):
+    print(n1+n2)
+
+def subtrair(n1,n2):
+    print(n1-n2)
+
+def multiplica(n1,n2):
+    print(n1*n2)
+
+def divisao(n1,n2):
+    print(n1/n2)
+
+#execução das funções
+soma(10,30)
+subtrair(30,20)
+multiplica(10,2)
+divisao(20,2)
+
