78a100cf2e5dab94a9c62ac0c232c2e23da60247
Daniel Papa <almeidapapa@gmail.com>
2023-02-26 09:54:23 -0500

    Exercicio 4 - Lista 01

---
+++ lista1_exercicio4.py
@@ -0,0 +1,21 @@
+## WS PRIME - LISTA DE EXERCÍCIOS Nº1 ##
+
+# EXERCÍCIO 04 #
+
+def soma(n1,n2,n3,n4):
+    print(n1+n2+n3+n4)
+
+def subtrair(n1,n2,n3,n4):
+    print(n1-n2-n3-n4)
+
+def multiplica(n1,n2,n3,n4):
+    print(n1*n2*n3*n4)
+
+def divisao(n1,n2,n3,n4):
+    print(n1/n2/n3/n4)
+
+#execução das funções
+soma(10,30,100,35)
+subtrair(30,20,2,4)
+multiplica(10,2,2,1)
+divisao(20,4,2,1)
