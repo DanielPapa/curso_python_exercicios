c484994797fde6cae2a216771b0507cd899d7c0f
Daniel Papa <almeidapapa@gmail.com>
2023-02-25 20:27:18 -0500

    Exercicio 02 - Lista 01

---
+++ lista1_exercicio2.py
@@ -0,0 +1,25 @@
+## WS PRIME - LISTA DE EXERCÍCIOS Nº1 ##
+
+# EXERCÍCIO 02 #
+
+n1 = input ("Digite um valor para n1: ")
+n2 = input ("Digite um valor para n2: ")
+
+n1 = float(n1)
+n2 = float(n2)
+
+soma = (n1 + n2)
+print (f' A soma das variáveis digitadas é igual a {soma}')
+subtração = (n1 - n2)
+print (f' A subtração das variáveis digitadas é igual a {subtração}')
+divisão = (n1 / n2)
+print (f' A divisão das variáveis digitadas é igual a {divisão}')
+multiplicação = (n1 * n2)
+print (f' A multiplicação das variáveis digitadas é igual a {multiplicação}')
+
+print ("Programa encerrado")
+
+
+## dúvida: como executar os comandos abaixo no Visual Code e obter a resposta do tipo de variável.
+# type(n1)
+# type (n2)
---
+++ teste2.py
@@ -0,0 +1,7 @@
+n1 = input ("Digite um valor para n1: ")
+n2 = input ("Digite um valor para n2: ")
+
+n1 = float(n1)
+n2 = float(n2)
+type(n1)
+type (n2)
