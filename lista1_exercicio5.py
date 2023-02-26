b2d5de1c78e78caa7eef050417760f4be485a616
Daniel Papa <almeidapapa@gmail.com>
2023-02-26 10:59:49 -0500

    Exercício 05 - Lista 01

---
+++ lista1_exercicio5.py
@@ -0,0 +1,33 @@
+## WS PRIME - LISTA DE EXERCÍCIOS Nº1 ##
+
+# EXERCÍCIO 05 #
+
+#n1 = input("digite um valor para n1:")
+#n2 = input("digite um valor para n2:")
+#n3 = input("digite um valor para n3:")
+
+#if n1>n2:
+ #   print(f' {n1} é o maior dos 3 valores digitados')
+
+#elif n2>n3:
+#    print (f' {n2} é o maior dos 3 valores digitados')
+#
+#else:
+#    print (f' {n3} é o maior dos 3 valores digitados')
+
+x = 20
+y = 2
+z = 2
+
+if x > y and x > z:
+    print("x é o maior número")
+
+elif y > x and y > z:
+    print ("y é o maior número")
+
+else:
+    print("z é o maior número")
+
+
+## dúvida:
+# Caso os número sejam iguais, como o programa pode interpretar essa igualdade e responder corretamente?
